ca97dd0ad4f45a0255844e39a1d0a750a6ab6eb0
Andrew Giese <andrewrgiese@gmail.com>
2019-01-28 11:12:37 -0800

    Created full clickable map and added territory highlighting for North America.

--- gs_play.py
+++ gs_play.py
@@ -6,9 +6,54 @@ from gsm import Gamestate
 from player import Player
 
 TERRITORY = {
-    (226, 217, 40, 255): "Alaska",
-    (214, 171, 128, 255): "Brazil",
-    (255, 252, 0, 255): "greenland.png"
+    # North America
+    (255, 255, 0, 255): "greenland_highlight.png",
+    (255, 255, 1, 255): "nwter_highlight.png",
+    (255, 255, 2, 255): "alaska_highlight.png",
+    (255, 255, 3, 255): "alberta_highlight.png",
+    (255, 255, 4, 255): "ontario_highlight.png",
+    (255, 255, 5, 255): "easterncan_highlight.png",
+    (255, 255, 6, 255): "westus_highlight.png",
+    (255, 255, 7, 255): "eastus_highlight.png",
+    (255, 255, 8, 255): "centralamerica_highlight.png",
+    # South America
+    (255, 127, 0, 255): "Argentina",
+    (255, 127, 1, 255): "Brazil",
+    (255, 127, 2, 255): "Peru",
+    (255, 127, 3, 255): "Venezuela",
+    # Africa
+    (127, 127, 127, 255): "North Africa",
+    (128, 128, 128, 255): "Egypt",
+    (129, 129, 129, 255): "East Africa",
+    (130, 130, 130, 255): "Central Africa",
+    (131, 131, 131, 255): "South Africa",
+    (132, 132, 132, 255): "Madagascar",
+    # Europe
+    (0, 0, 255, 255): "Iceland",
+    (1, 0, 255, 255): "Great Britain",
+    (2, 0, 255, 255): "Scandanavia",
+    (3, 0, 255, 255): "Russia",
+    (4, 0, 255, 255): "Northern Europe",
+    (5, 0, 255, 255): "Western Europe",
+    (6, 0, 255, 255): "Southern Europe",
+    # Asia
+    (0, 255, 0, 255): "Kamchatka",
+    (1, 255, 0, 255): "Yakutsk",
+    (2, 255, 0, 255): "Siberia",
+    (3, 255, 0, 255): "Ural",
+    (4, 255, 0, 255): "Irkutsk",
+    (5, 255, 0, 255): "Mongolia",
+    (6, 255, 0, 255): "Japan",
+    (7, 255, 0, 255): "Afghanistan",
+    (8, 255, 0, 255): "China",
+    (9, 255, 0, 255): "Middle East",
+    (10, 255, 0, 255): "India",
+    (11, 255, 0, 255): "Southeast Asia",
+    # Australia
+    (255, 0, 255, 255): "Indonesia",
+    (255, 1, 255, 255): "New Guinea",
+    (255, 2, 255, 255): "Western Australia",
+    (255, 3, 255, 255): "Eastern Australia"
 }
 MAX_PLAYERS = 4
 
@@ -34,7 +79,7 @@ class GSPlay(Gamestate):
     def initialize(self, core):
         self.core = core
         
-        self.background = core.load_image('continents.png')
+        self.background = core.load_image('continents_v2.png')
         self.background = pygame.transform.scale(self.background,
                                                  (core.display_width,
                                                   core.display_height))
@@ -71,11 +116,15 @@ class GSPlay(Gamestate):
             pos = self.core.get_mouse_pos()
             color = tuple(self.background.get_at(pos))
 
-            # zoom in on a continent when clicked
+            # highlight a territory when clicked
             if TERRITORY.get(color):
                 print("pos: {0}, color: {1}, state: {2}".format(
                     pos, color, TERRITORY.get(color)))
                 self.country = self.core.load_image(TERRITORY.get(color))
+                if self.country:
+                    self.country = pygame.transform.scale(self.country,
+                                                          (self.core.display_width,
+                                                           self.core.display_height))
 
             # slide player card when clicked
             for player in self.players:
